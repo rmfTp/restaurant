26decfa1cfca63f69edcb4bd0b95348fb4add956
rmftp <kyj2001kyj@naver.com>
2025-07-18 12:57:21 +0900

    kakao api 연결 test

--- src/test/java/org/koreait/member/social/test/AccessTokenTest.java
+++ src/test/java/org/koreait/member/social/test/AccessTokenTest.java
@@ -1,6 +1,7 @@
 package org.koreait.member.social.test;
 
 import org.junit.jupiter.api.Test;
+import org.koreait.member.social.entities.AuthToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
@@ -9,6 +10,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Map;
 
 @SpringBootTest
 public class AccessTokenTest {
@@ -24,14 +26,27 @@ public class AccessTokenTest {
         body.add("grant_type", "authorization_code");
         body.add("client_id", "554171db1e052c89471da8f35fb9e6ac");
         body.add("redirect_uri", "http://localhost:3000/member/social/callback/kakao");
-        body.add("code", "zF_CVgRkubvcdNWUuITuxSTAfZ69xojlq5X9Xfv4KLXSeCXQ2ioHZAAAAAQKFwYuAAABmBuFXNHE017PSiBv1Q");
+        body.add("code", "QT090_D-Sl6cXOmwhIUmUY5Cx4ZpY61HB6lP8DE6zw_eCxYVnpw3YQAAAAQKDSCbAAABmButIFP7Ewsnpgvovw ");
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body,headers);
 
         String requestUrl = "https://kauth.kakao.com/oauth/token";
 
-        ResponseEntity<String> response = restTemplate.exchange(URI.create(requestUrl), HttpMethod.POST, request, String.class);
+        ResponseEntity<AuthToken> response = restTemplate.exchange(URI.create(requestUrl), HttpMethod.POST, request, AuthToken.class);
         HttpStatusCode status = response.getStatusCode();
         System.out.println("status: " + status);
         System.out.println(response.getBody());
+
+        AuthToken authToken = response.getBody();
+        requestUrl = "https://kapi.kakao.com/v2/user/me";
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBearerAuth(authToken.getAccessToken());
+
+        request = new HttpEntity<>(headers);
+        ResponseEntity<Map> res = restTemplate.exchange(URI.create(requestUrl), HttpMethod.POST, request, Map.class);
+
+        Map resBody = res.getBody();
+        long id = (Long)resBody.get("id");
+        System.out.println(id);
     }
 }
