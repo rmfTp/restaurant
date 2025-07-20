f8e05146bdf426a9cf2b8642b27efb15be283e1b
rmfTq <kyj2001kyj@naver.com>
2025-07-20 20:53:05 +0900

    배포

--- src/main/java/org/koreait/SecondhandApplication.java
+++ src/main/java/org/koreait/SecondhandApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecondhandApplication {
 /*환경변수
 db.password=1234;db.url=localhost:3306/SECONDHAND;db.username=SECONDHAND;ddl.auto=create;file.path=C:/uploads;file.url=/uploads;kakao.apikey=554171db1e052c89471da8f35fb9e6ac;python.base=C:/trend/.venv/Scripts;python.base2=C:/restaurant/.venv/Scripts;python.restaurant=C:/restaurant;python.trend=C:/trend
- */
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(SecondhandApplication.class, args);
 	}
